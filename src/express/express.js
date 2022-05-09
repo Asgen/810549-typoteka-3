b99644c59acab4b491b009a7e3872f6f21203f48
Геннадий Цаюков <genuus1@gmail.com>
2022-05-09 13:51:09 +0300

    fix reviews

--- src/express/express.js
+++ src/express/express.js
@@ -2,11 +2,15 @@
 
 const express = require(`express`);
 const mainRoutes = require(`./routes/main-routes`);
+const articlesRoutes = require(`./routes/main-routes`);
+const myRoutes = require(`./routes/main-routes`);
 
 const DEFAULT_PORT = 8081;
 
 const app = express();
 app.use(`/`, mainRoutes);
+app.use(`/`, articlesRoutes);
+app.use(`/`, myRoutes);
 
 app.listen(DEFAULT_PORT);
 
