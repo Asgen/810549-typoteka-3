67887e3bb6e5e815a0e51332cddd8794a48fa173
Gennadii Tsaiukov <g.tsaiukov@sxope.com>
2022-10-17 21:45:41 +0300

    wip

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -2,21 +2,25 @@
 
 const chalk = require(`chalk`);
 const fs = require(`fs/promises`);
+const {nanoid} = require(`nanoid`);
 
 const {getRandomInt, shuffle, readContent} = require(`../../utils`);
 
 const DEFAULT_COUNT = 1;
 const MAX_COUNT = 1000;
+const MAX_ID_LENGTH = 4;
 const FILE_NAME = `mocks.json`;
 
 const FILE_SENTENCES_PATH = `data/sentences.txt`;
 const FILE_TITLES_PATH = `data/titles.txt`;
+const FILE_COMMENTS_PATH = `data/comments.txt`;
 const FILE_CATEGORIES_PATH = `data/categories.txt`;
 
-const generate = (count, titles, categories, sentences) =>
+const generate = (count, titles, categories, sentences, comments) =>
   Array(count)
     .fill({})
     .map(() => ({
+      id: nanoid(MAX_ID_LENGTH),
       title: titles[getRandomInt(0, titles.length - 1)],
       announce: shuffle(sentences).slice(0, 5).join(` `),
       fullText: shuffle(sentences)
@@ -27,6 +31,10 @@ const generate = (count, titles, categories, sentences) =>
           getRandomInt(0, categories.length - 1)
       ),
       createdDate: new Date().toISOString(),
+      comments: comments.slice(0, getRandomInt(1, 5)).map(() => ({
+        id: nanoid(MAX_ID_LENGTH),
+        text: shuffle(comments).slice(0, getRandomInt(1, 3)).join(` `)
+      }))
     }));
 
 module.exports = {
@@ -35,10 +43,10 @@ module.exports = {
     const sentences = await readContent(FILE_SENTENCES_PATH);
     const titles = await readContent(FILE_TITLES_PATH);
     const categories = await readContent(FILE_CATEGORIES_PATH);
+    const comments = await readContent(FILE_COMMENTS_PATH);
 
     const [count] = args;
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
-
     if (countOffer > MAX_COUNT) {
       console.info(chalk.red(`Не больше ${MAX_COUNT} публикаций`));
 
@@ -47,7 +55,7 @@ module.exports = {
       return;
     }
 
-    const content = JSON.stringify(generate(countOffer, titles, categories, sentences), null, 4);
+    const content = JSON.stringify(generate(countOffer, titles, categories, sentences, comments), null, 4);
 
     try {
       await fs.writeFile(FILE_NAME, content);
